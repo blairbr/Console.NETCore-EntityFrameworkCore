1d9a835218213215c817601b3c7a3f1267aa3126
unknown <blair.j.brown@gmail.com>
2020-02-21 22:31:07 -0500

    added update in main method to insert a Blog record to DB

--- CodeFirstNewDatabaseSample/Program.cs
+++ CodeFirstNewDatabaseSample/Program.cs
@@ -10,6 +10,23 @@ namespace CodeFirstNewDatabaseSample
     {
         static void Main(string[] args)
         {
+            //in a web app this insert would be tied to a method called to an onclick event 
+            // Disconnected Student entity
+            Blog blog1 = new Blog() { Url = "http://url.com" };
+
+            blog1.Rating = 10;
+
+            using (var context = new BloggingContext())
+            {
+                context.Update<Blog>(blog1);
+
+                // or the followings are also valid
+                // context.Blog.Update(blog1);
+                // context.Attach<Blog>(blog1).State = EntityState.Modified;
+                // context.Entry<Blog>(blog1).State = EntityState.Modified; 
+
+                context.SaveChanges();
+            }
         }
     }
     public class BloggingContext : DbContext
@@ -23,7 +40,8 @@ namespace CodeFirstNewDatabaseSample
                 @"Server=(localdb)\mssqllocaldb;Database=CodeFirstNewDatabaseSample;Trusted_Connection=True");
               //  @"Server=localhost\SQLEXPRESS01;Database=master;Trusted_Connection=True");
         }
-    }
+
+}
 
     public class Blog
     {
@@ -31,7 +49,8 @@ namespace CodeFirstNewDatabaseSample
         public string Url { get; set; }
         public int Rating { get; set; }
         public List<Post> Posts { get; set; }
-    }
+
+}
 
     public class Post
     {
